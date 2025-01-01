0933d56978da224c887ace846ed85e15758135a6
Ben Rees <ben@rees.biz>
2025-01-01 14:49:41 +1300

    Add new Ensure helper class

---
+++ EnsureFailedException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace Rees.UnitTestUtilities;
+
+[Serializable]
+public class EnsureFailedException : Exception
+{
+    public EnsureFailedException()
+    {
+    }
+
+    public EnsureFailedException(string message) : base(message)
+    {
+    }
+
+    public EnsureFailedException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+    protected EnsureFailedException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
